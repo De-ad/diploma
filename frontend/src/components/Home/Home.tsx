9739fa391535885a77016d2577f815954f928acd
Dead <space.gee.z@gmail.com>
2025-03-11 19:59:11 +0300

    Add website URL input and validation

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateURL } from "../../utils/checkWebsiteURL";
 
 export const Home = () => {
-  return <div>hi</div>;
+  const [websiteURL, setWebsiteURL] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleAudit = () => {
+    if (validateURL(websiteURL)) {
+      setErrorMessage("");
+    } else {
+      setErrorMessage("URL сайта не валидный");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Введите URL сайта"
+        onChange={(e) => setWebsiteURL(e.target.value)}
+        value={websiteURL}
+      ></input>
+      <button onClick={handleAudit}>Проанализировать</button>
+      <div>{errorMessage}</div>
+    </div>
+  );
 };
---
+++ frontend/src/utils/checkWebsiteURL.ts
@@ -0,0 +1,15 @@
+export const getWebsiteNameFromURL = (URL: string, str: URL) => {};
+
+export function validateURL(str: string) {
+  str = str.trimEnd();
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i",
+  ); 
+  return !!pattern.test(str);
+}
