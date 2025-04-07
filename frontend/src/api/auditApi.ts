13e48741520470ce0d308d900176d7895f02a253
Dead <space.gee.z@gmail.com>
2025-04-07 17:38:43 +0300

    Add axios instance

--- frontend/src/api/auditApi.ts
+++ frontend/src/api/auditApi.ts
@@ -1,17 +1,13 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({
-//     baseURL: "https://your-api.com/api", /
-//     timeout: 10000,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:8001",
+  timeout: 10000,
+});
 
 export const sendData = async (websiteURL: string, websiteImages?: []) => {
   const formData = new FormData();
   formData.append("websiteURL", websiteURL);
-  console.log("he");
   if (websiteImages) {
     for (let i = 0; i < websiteImages.length; i++) {
       const response = await fetch(websiteImages[i].dataURL);
@@ -20,17 +16,16 @@ export const sendData = async (websiteURL: string, websiteImages?: []) => {
       formData.append("image", file);
     }
   }
-  const response = await axios
+  const response = await axiosInstance
     .post("/test", formData, {
       headers: {
         "Content-type": "multipart/form-data",
       },
     })
-    .then(function (response) {
-      console.log(response);
-    })
     .catch(function (error) {
       console.log(error);
     });
+
+  console.log(response.data);
   return response.data;
 };
