3824f77b240aec4538348f4471e72ed91507dbd8
Dead <space.gee.z@gmail.com>
2025-04-07 18:18:42 +0300

    Add multiple images request

--- backend/llm/main.py
+++ backend/llm/main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from pydantic import BaseModel
+from typing import List
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
-
+import base64
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+import sys
 
 app = FastAPI()
 load_dotenv()
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True) 
@@ -28,8 +47,27 @@ class Image(BaseModel):
 
 @app.get("/")
 def read_root():
+    payload = []
+    payload.append({
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{1}",
+                    },
+                ],
+            } )
+    payload.append({
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{2}",
+                    },
+                ],
+            } )
+    logger.debug(payload)
     return {"Hello": "Ping"}
-
 
 @app.post("/llm/analyze")
 def analyze_code():
@@ -37,21 +75,42 @@ def analyze_code():
 
 
 @app.post("/vlm/analyze")
-def analyze_images(image: Image):
+async def analyze_images(websiteURL: str = Form(...), image: List[UploadFile] = File(default=[])):
 
-    messages = [
-    {"role": "system", "content": "You are a designer of websites. You will be provided with an image of a website. Your goal will be to evaluate a website photo according following criteria: color scheme, typography, spacing & alignment. For each attribute provide the output and give a final grade from 0 to 100"},
-    {
+    results = {
+        "websiteURL": websiteURL,
+        "images": []
+    }
+    
+    payload = []
+
+    for img in image:
+        content = await img.read()
+        base64_image = base64.b64encode(content).decode("utf-8")
+        results["images"].append({
+            "filename": img.filename,
+            "content_type": img.content_type,
+            "base64": base64_image
+        })
+        payload.append({
                 "role": "user",
                 "content": [
                     {
                         "type": "image_url",
-                        "image_url": f"data:image/jpeg;base64,{image.base64_string}",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
                     },
                 ],
-            } 
+            } )
+        
+
+    
+    messages = [
+    {"role": "system", "content": "You are a designer of websites. You will be provided with an image of a website. Your goal will be to evaluate a website photo according following criteria: color scheme, typography, spacing & alignment. For each attribute provide the output and give a final grade from 0 to 100"}
     ]
     
+
+    messages = messages + payload
+    logger.debug(messages)
         
     response =  client.chat.completions.create(  
         model="vis-openai/gpt-4o-2024-08-06",
@@ -92,4 +151,4 @@ def analyze_images(image: Image):
 
     
 
-    return {"name": image.name, "message": response.choices[0].message.content}
+    return {"message": response.choices[0].message.content}
