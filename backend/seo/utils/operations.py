790ef42483f300c599e9ab869eb4419dcb2cdbd8
Dead <space.gee.z@gmail.com>
2025-04-15 19:44:49 +0300

    Add word cloud

--- backend/seo/utils/operations.py
+++ backend/seo/utils/operations.py
@@ -1,8 +1,14 @@
+from bs4 import BeautifulSoup
 import httpx
 import socket
 import ssl
 import re
 import os
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
 
 async def check_if_robots_file_is_present(url: str):
     url = f"{url.rstrip('/')}/robots.txt"
@@ -101,8 +107,36 @@ async def check_performance_metrics(url: str):
         except httpx.RequestError as e:
             return {"error": f"Request failed: {str(e)}"}
 
-def create_word_cloud():
-    return 
+async def create_word_cloud(url: str):
+     async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                for tag in soup(['script', 'style', 'noscript']):
+                    tag.decompose()
+
+                text = soup.get_text(separator=' ', strip=True)
+                wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+
+                img_io = io.BytesIO()
+                wordcloud.to_image().save(img_io, format='PNG')
+                img_io.seek(0)
+
+                img_base64 = base64.b64encode(img_io.read()).decode('utf-8')
+                return {'image': f"data:image/png;base64,{img_base64}"}
+                
+            else:
+                return {
+                    "message": "error"
+                }
+        except httpx.RequestError as e:
+            return {"error": str(e)}
+
+  
+
+   
 
 # inline css, metadata, deprecated, headers structure, title, description, favicon, image seo, 
 def check_http_code(url):
