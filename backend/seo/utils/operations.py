5366960e8e25b7a8198e649784f2321369190b5f
Dead <space.gee.z@gmail.com>
2025-04-15 16:30:19 +0300

    Add metrics api

--- backend/seo/utils/operations.py
+++ backend/seo/utils/operations.py
@@ -2,6 +2,7 @@ import httpx
 import socket
 import ssl
 import re
+import os
 
 async def check_if_robots_file_is_present(url: str):
     url = f"{url.rstrip('/')}/robots.txt"
@@ -59,9 +60,49 @@ def check_ssl_certificate(url: str):
                     "result": "has invalid cert" +str(e)
                 }
 
-def check_performance_metrics():
-    return
+async def check_performance_metrics(url: str):
+    api_key = os.getenv("API_KEY")
+    api_url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={url}&key={api_key}"
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(api_url)
+            if response.status_code == 200:
+                try:
+                    data = response.json()
+                except ValueError:
+                    return {"error": "Invalid JSON response", "raw": response.text}
 
+                if 'error' in data:
+                    return {"error": data["error"]["message"], "details": data["error"]}
+
+                try:
+                    performance_score = data['lighthouseResult']['categories']['performance']['score'] * 100
+                    fcp = data['lighthouseResult']['audits']['first-contentful-paint']['displayValue']
+                    lcp = data['lighthouseResult']['audits']['largest-contentful-paint']['displayValue']
+                    cls = data['lighthouseResult']['audits']['cumulative-layout-shift']['displayValue']
+                    tbt = data['lighthouseResult']['audits']['total-blocking-time']['displayValue']
+
+                    return {
+                        "performanceScore": performance_score,
+                        "firstContentfulPaint": fcp,
+                        "largestContentfulPaint": lcp,
+                        "cumulativeLayoutShift": cls,
+                        "totalBlockingTime": tbt
+                    }
+                except KeyError as e:
+                    return {"error": f"Missing expected data in response: {e}", "data": data}
+
+            else:
+                return {
+                    "status_code": response.status_code,
+                    "message": "Non-200 response",
+                    "body": response.text
+                }
+        except httpx.RequestError as e:
+            return {"error": f"Request failed: {str(e)}"}
+
+def create_word_cloud():
+    return 
 
 # inline css, metadata, deprecated, headers structure, title, description, favicon, image seo, 
 def check_http_code(url):
