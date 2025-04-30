19919cb15f6fc93cb1f4f7cafc9ec25f0938dec7
Dead <space.gee.z@gmail.com>
2025-04-30 13:53:34 +0300

    Add format script

--- backend/llm/main.py
+++ backend/llm/main.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 import base64
 from fastapi.middleware.cors import CORSMiddleware
 import logging
-import sys
 
 app = FastAPI()
 load_dotenv()
@@ -24,7 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-logger = logging.getLogger('uvicorn.error')
+logger = logging.getLogger("uvicorn.error")
 logger.setLevel(logging.DEBUG)
 
 # openai_key from env
@@ -34,8 +33,8 @@ openai_key = os.getenv("API_KEY")
 client = OpenAI(
     api_key=openai_key,
     base_url="https://api.vsegpt.ru/v1",
-
 )
+
 
 class Image(BaseModel):
     name: str
@@ -45,26 +44,31 @@ class Image(BaseModel):
 @app.get("/")
 def read_root():
     payload = []
-    payload.append({
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image_url",
-                        "image_url": f"data:image/jpeg;base64,{1}",
-                    },
-                ],
-            } )
-    payload.append({
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image_url",
-                        "image_url": f"data:image/jpeg;base64,{2}",
-                    },
-                ],
-            } )
+    payload.append(
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{1}",
+                },
+            ],
+        }
+    )
+    payload.append(
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": f"data:image/jpeg;base64,{2}",
+                },
+            ],
+        }
+    )
     logger.debug(payload)
     return {"Hello": "Ping"}
+
 
 @app.post("/llm/analyze")
 def analyze_code():
@@ -72,24 +76,25 @@ def analyze_code():
 
 
 @app.post("/vlm/analyze")
-async def analyze_images(websiteURL: str = Form(...), image: List[UploadFile] = File(default=[])):
+async def analyze_images(
+    websiteURL: str = Form(...), image: List[UploadFile] = File(default=[])
+):
+    results = {"websiteURL": websiteURL, "images": []}
 
-    results = {
-        "websiteURL": websiteURL,
-        "images": []
-    }
-    
     payload = []
 
     for img in image:
         content = await img.read()
         base64_image = base64.b64encode(content).decode("utf-8")
-        results["images"].append({
-            "filename": img.filename,
-            "content_type": img.content_type,
-            "base64": base64_image
-        })
-        payload.append({
+        results["images"].append(
+            {
+                "filename": img.filename,
+                "content_type": img.content_type,
+                "base64": base64_image,
+            }
+        )
+        payload.append(
+            {
                 "role": "user",
                 "content": [
                     {
@@ -97,55 +102,54 @@ async def analyze_images(websiteURL: str = Form(...), image: List[UploadFile] = 
                         "image_url": f"data:image/jpeg;base64,{base64_image}",
                     },
                 ],
-            } )
-        
+            }
+        )
 
-    
     messages = [
-    {"role": "system", "content": "You are a designer of websites. You will be provided with an image of a website. Your goal will be to evaluate a website photo according following criteria: color scheme, typography, spacing & alignment. For each attribute provide the output and give a final grade from 0 to 100"}
+        {
+            "role": "system",
+            "content": "You are a designer of websites. You will be provided with an image of a website. Your goal will be to evaluate a website photo according following criteria: color scheme, typography, spacing & alignment. For each attribute provide the output and give a final grade from 0 to 100",
+        }
     ]
-    
 
     messages = messages + payload
     logger.debug(messages)
-        
-    response =  client.chat.completions.create(  
+
+    response = client.chat.completions.create(
         model="vis-openai/gpt-4o-2024-08-06",
         messages=messages,
         n=1,
         max_tokens=500,
         response_format={
-                "type": "json_schema",
-                "json_schema": {
-                    "name": "visual_analysis",
-                    "schema": {
-                        "type": "object",
-                        "properties": {
-                            "criteria": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {
-                                        "attribute": {"type": "string"},
-                                        "output": {"type": "string"}
-                                    },
-                                    "required": ["attribute", "output"],
-                                    "additionalProperties": False
-                                }
+            "type": "json_schema",
+            "json_schema": {
+                "name": "visual_analysis",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "criteria": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "attribute": {"type": "string"},
+                                    "output": {"type": "string"},
+                                },
+                                "required": ["attribute", "output"],
+                                "additionalProperties": False,
                             },
-                            "final_grade": {"type": "string"}
                         },
-                        "required": ["criteria", "final_grade"],
-                        "additionalProperties": False
+                        "final_grade": {"type": "string"},
                     },
-                    "strict": True
-                }
-            }
+                    "required": ["criteria", "final_grade"],
+                    "additionalProperties": False,
+                },
+                "strict": True,
+            },
+        },
     )
 
     print("Response:", response)
     print(response.choices[0].message.content)
-
-    
 
     return {"message": response.choices[0].message.content}
